3181f6effdd720f46778e777c633b9fcc116c2fa
xkarx <kartikeya@btech.christuniversity.in>
2020-02-21 00:00:47 +0530

    Layout proper, page goes blank after clicking on an option

--- Quizey/quizey/src/components/QuestionBox.js
+++ Quizey/quizey/src/components/QuestionBox.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, {useState} from "react";
 
-const QuestionBox = ({question, options}) => {
+//for managing state in a function component, react offers a plug and play API known as HOOKS
+// thus useState method is imported above
+
+// selected used below is a prop
+const QuestionBox = ({question, options, selected}) => {
+  const [answer, setAnswer] = useState(options); //creating state variable answer, initial value set from the options
+  // setAnswer is used to update the value of the variable
   return (
     <div className="questionBox">
       <div className="question">{question}</div>
+      
+    {// rendering a series of buttons
+    }
+	  {answer.map((text, index) => (
+        <button
+          key={index}
+          className="answerBtn"
+          onClick={() => {
+            setAnswer([text]);
+            selected(text);
+          }}
+        >
+          {text}
+        </button>
+      ))}
     </div>
   );
 };
--- Quizey/quizey/src/index.js
+++ Quizey/quizey/src/index.js
@@ -4,11 +4,17 @@ import "./assets/style.css";
 
 import quizService from "./quizService";
 
-import QuestionBox from "./components/QuestionBox"
+import QuestionBox from "./components/QuestionBox";
+
+import Result from "./components/Result";
+
 
 class Quizey extends Component{
     state = {
-        questionBank : [] //Instantiating local state within the array
+        questionBank : [], //Instantiating local state within the array
+        score: 0,
+        responses: 0
+
     }; //this type of declaration is possible in js, thanks to babel
 
     //Invoking quizService
@@ -19,6 +25,18 @@ class Quizey extends Component{
             });
         });
     };
+
+    //check if users answer matches the correct answer, if yes then update count
+    computeAnswer = (answer, correctAnswer) => {
+        if(answer === correctAnswer) {
+            this.setState({
+                score: this.state.score + 1
+            });
+        }
+        this.setState({
+            responses: this.state.responses < 5 ? this.state.rsponses + 1 : 5
+        })
+    }
 
     //to run the above when a component loads up, componentDidMount LifeCycleMethod is used
     componentDidMount(){
@@ -35,16 +53,20 @@ class Quizey extends Component{
                }
 
                {    this.state.questionBank.length > 0 && 
+                    this.state.responses < 5 &&
                     this.state.questionBank.map(
                         ({question, answers, correct, questionId}) => (
                             <QuestionBox 
                                 question = {question} 
                                 options = {answers} 
                                 key={questionId}
+                                selected = {answer => this.computeAnswer(answer,correct)}
                             />
                         )
                     
                     )}
+                    {this.state.responses === 5? (<Result score = {this.state.score} playAgain= {this.playAgain} />
+                        )    :null}
             </div>
         );
     }
