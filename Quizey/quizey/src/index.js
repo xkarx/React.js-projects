cefac4a955672dd990fed23488c78b51765a7dfd
xkarx <kartikeya@btech.christuniversity.in>
2020-02-20 23:07:43 +0530

    functionality added to render 5 different questions(only) each time

--- Quizey/quizey/src/index.js
+++ Quizey/quizey/src/index.js
@@ -2,12 +2,38 @@ import React, {Component} from "react";
 import ReactDOM from "react-dom";
 import "./assets/style.css";
 
+import quizService from "./quizService";
+
 class Quizey extends Component{
+    state = {
+        questionBank : [] //Instantiating local state within the array
+    }; //this type of declaration is possible in js, thanks to babel
+
+    //Invoking quizService
+    getQuestions = () => {
+        quizService().then(question => {
+            this.setState({
+                questionBank: question
+            });
+        });
+    };
+
+    //to run the above when a component loads up, componentDidMount LifeCycleMethod is used
+    componentDidMount(){
+        this.getQuestions();
+    }
+
     render(){
         return(
             <div className = "container">
                 <div className = "title">Quizey</div>
                {//“How somebody is different?? Not different but genius?? (Find the answer, that's my quiz for you... No, jokers it's to easy with jokers)”
+               }
+               {// to render componentDidMount()
+                    this.state.questionBank.length > 0 && 
+                    this.state.questionBank.map(
+                        ({question, answers, correct, questionId}) => <h4>{question}</h4>
+                    )
                }
             </div>
         );
